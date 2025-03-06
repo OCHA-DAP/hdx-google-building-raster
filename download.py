40b2a73b1be4d19cc8e83e32c2ffa3a8e8dd7534
Max Malynowsky <github@maxmalynowsky.com>
2025-03-06 17:13:25 +0100

    convert client to streaming download

--- download.py
+++ download.py
@@ -3,13 +3,13 @@ from pathlib import Path
 
 import duckdb
 from dotenv import load_dotenv
-from httpx import Client
+from httpx import stream
 
 load_dotenv(override=True)
 
 GEOJSON = getenv("DOWNLOAD_GEOJSON", "")
 YEAR = getenv("DOWNLOAD_YEAR", "")
-TIMEOUT = int(getenv("DOWNLOAD_TIMEOUT", "600"))
+CHUNK_SIZE = int(getenv("CHUNK_SIZE", "104857600"))
 
 cwd = Path(__file__).parent
 stac_path = cwd / "data/stac.parquet"
@@ -29,14 +29,12 @@ def main() -> None:
                 geometry
             ) AND date_part('year', datetime) = {YEAR};
         """
-        download_urls = con.sql(query).to_df()["href"].tolist()
-    for download_url in download_urls:
-        file_name = download_url.split("/")[-1]
-        with Client(http2=True, timeout=TIMEOUT) as client:
-            response = client.get(download_url)
-            response.raise_for_status()
-            with (download_path / file_name).open("wb") as f:
-                f.write(response.content)
+        urls = con.sql(query).to_df()["href"].tolist()
+    for url in urls:
+        file_name = url.split("/")[-1]
+        with (download_path / file_name).open("wb") as f, stream("GET", url) as r:
+            for data in r.iter_bytes(chunk_size=CHUNK_SIZE):
+                f.write(data)
 
 
 if __name__ == "__main__":
